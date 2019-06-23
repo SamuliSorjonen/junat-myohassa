104f8a60587cb8e1d1e0f2151903911def8ac0d8
teepli <48477074+teepli@users.noreply.github.com>
2019-06-23 22:25:09 +0300

    styling changes

--- koodi.js
+++ koodi.js
@@ -104,17 +104,17 @@ function renderData(data) {
 }
 
 
-window.onscroll = function () {
-    myFunction()
-};
-
-let header = document.getElementById("myHeader");
-let sticky = header.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
-    }
-}
+// window.onscroll = function () {
+//     myFunction()
+// };
+//
+// let header = document.getElementById("myHeader");
+// let sticky = header.offsetTop;
+//
+// function myFunction() {
+//     if (window.pageYOffset > sticky) {
+//         header.classList.add("sticky");
+//     } else {
+//         header.classList.remove("sticky");
+//     }
+// }
